b868f3e69c3aca7c51bffbf6553c0a32e15c62b4
adamjames870 <adamjames870@yahoo.co.uk>
2025-03-30 12:07:09 +0800

    Working report

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 from stats import count_words, count_chars_and_sort
+import sys
 
 def main():
-    PATH_FRANKENSTEIN = "books/frankenstein.txt"
-    book_text = get_book_text(PATH_FRANKENSTEIN)
+    
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    # PATH_FRANKENSTEIN = "books/frankenstein.txt"
+    
+    path = sys.argv[1]
+    book_text = get_book_text(path)
     word_count = count_words(book_text)
-    print(f"{word_count} words found in the document")
     char_count = count_chars_and_sort(book_text)
-    print(char_count )
+    
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    print_char_count(char_count)
+    print("============= END ===============")
 
 def get_book_text(path_to_file):
     file_contents = ""
     with open(path_to_file) as book_file:
         file_contents = book_file.read()
     return file_contents
+
+def print_char_count(char_dict):
+    for line in char_dict:
+        print(f"{line}: {char_dict[line]}")
 
 main()
--- stats.py
+++ stats.py
@@ -7,14 +7,20 @@ def count_characters(text_to_count):
     characters = {}
     text_to_count = text_to_count.lower()
     for char in text_to_count:
-        current_count = 0
-        try:
-            current_count = characters[char]
-        except KeyError as e:
-            pass
-        characters[char] = current_count + 1
+        if not is_ignorable(char):
+            current_count = 0
+            try:
+                current_count = characters[char]
+            except KeyError as e:
+                pass
+            characters[char] = current_count + 1
     return characters
 
+def is_ignorable(char):
+    # IGNORABLE_CHARS = [" ", "\n", ":", ",", ".", "?", ";", ":", "“", "”", "!", "-", "'", "_", "—", "*", "1", "2", "3"]
+    return not char.isalpha()
+
 def count_chars_and_sort(text_to_count):
-    unsort_dict = count_characters(text_to_count)
-    return unsort_dict
+    unsorted_dict = count_characters(text_to_count)
+    sorted_dict = {key: value for key, value in sorted(unsorted_dict.items(), key=lambda item: item[1], reverse=True)}
+    return sorted_dict
